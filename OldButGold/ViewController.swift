04543e7e9cc76f6ab1773939d0a92c5f0902bb1b
Ziyi Ye <ziy116@ucsd.edu>
2019-04-18 18:57:04 -0700

    Initial Commit

---
+++ OldButGold/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OldButGold
+//
+//  Created by Ziyi Ye on 4/18/19.
+//  Copyright © 2019 Frank Ye. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
