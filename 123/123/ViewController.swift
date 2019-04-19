034d47560b4efceb1ae663b4975a8ac89a6a7ecc
kwkarlwang <kawang@ucsd.edu>
2019-04-18 18:43:22 -0700

    1

---
+++ 123/123/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  123
+//
+//  Created by Karl Wang on 4/18/19.
+//  Copyright © 2019 kwkarlwang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
