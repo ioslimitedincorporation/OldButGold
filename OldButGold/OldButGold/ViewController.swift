78f11e6b5d952b84c6d9b30c24e8333f42e4d50b
onestarYX <40019226+onestarYX@users.noreply.github.com>
2019-04-18 18:58:49 -0700

    initial commit via upload

---
+++ OldButGold/OldButGold/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OldButGold
+//
+//  Created by Yixing Wang on 4/18/19.
+//  Copyright © 2019 wjfwyx@163.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
