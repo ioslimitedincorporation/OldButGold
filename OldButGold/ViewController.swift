ea0e76b659a7165df7ce416a06733097fcfc0779
Ziyi Ye <ziy116@ucsd.edu>
2019-05-02 22:52:47 -0700

    basic feature for displaying home screen

--- OldButGold/ViewController.swift
+++ OldButGold/ViewController.swift
@@ -7,32 +7,61 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
 
-
+    @IBOutlet weak var tableView: UITableView!
+    var ref: DatabaseReference!
+    var posts: [[String:Any]] = []
+    //var posts: [String:[String:Any]] = [:]
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 50 // TODO change to post count
+        return posts.count// TODO change to post count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
         
-        cell.titleLabel.text = "row:\(indexPath.row)"
-        cell.descriptionLabel.text = "TODO insert "
+        print(indexPath.row)
+        
+        let post = self.posts[indexPath.row]
+        print(post)
+    
+        cell.titleLabel.text = post["title"] as! String
+        //cell.titleLabel.text = posts[
+        cell.descriptionLabel.text = post["description"] as! String
         return cell
     }
     
 
+    func read(){
+        ref = Database.database().reference()
+        ref.child("Posts").observeSingleEvent(of: .value, with: { (snapshot) in
+            // Get user value
+            let dict = snapshot.value as? [String:[String:Any]]
+            
+         
+            for (key, value) in dict! {
+                self.posts.append(value)
+            }
+            print(self.posts)
+            self.tableView.reloadData()
+        }) { (error) in
+            print(error.localizedDescription)
+        }
+        
+        
+    }
     
     @IBOutlet weak var homeTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Hello")
-        
+        read()
         homeTableView.dataSource = self
         homeTableView.delegate = self
+        
         
             //update with database
         
