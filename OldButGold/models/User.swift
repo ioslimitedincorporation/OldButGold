65cbf3b7712118081f74d0ec86d10ede9b42136c
Ziyi Ye <ziy116@ucsd.edu>
2019-05-14 19:10:22 -0700

    some updates on profile page

---
+++ OldButGold/models/User.swift
@@ -0,0 +1,31 @@
+//
+//  User.swift
+//  OldButGold
+//
+//  Created by Ziyi Ye on 5/14/19.
+//  Copyright © 2019 Frank Ye. All rights reserved.
+//
+
+import Foundation
+
+class User{
+    var key: String
+    var email: String
+    var name: String
+    var post: [String]
+    
+    
+    init(dictionary: [String: AnyObject], key: String) {
+        self.key = key
+        self.email = dictionary["email"] as! String
+        self.name = dictionary["name"] as! String
+        let myPost = dictionary["post"] as! [String: String]
+        self.post = []
+        //TODO
+        
+        for key in Array(myPost.keys){
+            self.post.append(key)
+        }
+        
+    }
+}
