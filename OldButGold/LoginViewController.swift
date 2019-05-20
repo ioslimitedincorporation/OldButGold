c0fe87a3cf9611e80ab2858193a4547c2222c7a4
Ziyi Ye <ziy116@ucsd.edu>
2019-05-20 09:56:45 -0700

    UI updates

--- OldButGold/LoginViewController.swift
+++ OldButGold/LoginViewController.swift
@@ -22,6 +22,7 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    
     }
     
     @IBAction func onSignIn(_ sender: Any) {
@@ -49,26 +50,6 @@ class LoginViewController: UIViewController {
         }
     }
     
-//    @IBAction func onSignUp(_ sender: Any) {
-//        Auth.auth().createUser(withEmail: emailField.text!+"@ucsd.edu", password: passwordField.text!) { (user, error) in
-//            if error != nil{
-//                print(error!)
-//            }else{
-//                print("success")
-//            }
-//        }
-//        
-//    }
-    
-    
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
