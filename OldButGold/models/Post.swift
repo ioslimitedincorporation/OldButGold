082df6e3e424e57e32a46e2f17ac96e4b426326b
Yixing Wang <yxonestarw@gmail.com>
2019-05-25 01:08:55 -0700

    fixed images error

--- OldButGold/models/Post.swift
+++ OldButGold/models/Post.swift
@@ -32,21 +32,30 @@ class Post {
 
         self.description = dictionary["description"] as! String
         self.timestamp = dictionary["timestamp"] as? Double ?? 0
-        self.ref = Database.database().reference()
+//        self.ref = Database.database().reference()
         self.image = []
 
+        if let array = dictionary["images"] as? [String: String] {
+            if array != nil {
+                for url in array.values{
+                    self.image.append(url)
+                }
+            }
+        }
+
+
         
-        self.ref.child("Posts").child(key).child("images").observeSingleEvent(of: .value, with: { (snap) in
-            let array1 = snap.value as? [String:String]
-            
-            guard let array=array1 else {
-                print("no array for this post now")
-                return
-            }
-            for i in Array(array){
-                self.image.append(i.value)
-            
-            }
-        })
+//        self.ref.child("Posts").child(key).child("images").observeSingleEvent(of: .value, with: { (snap) in
+//            let array1 = snap.value as? [String:String]
+//
+//            guard let array=array1 else {
+//                print("no array for this post now")
+//                return
+//            }
+//            for i in Array(array){
+//                self.image.append(i.value)
+//
+//            }
+//        })
     }
 }
